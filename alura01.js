3dc88cb88a7fa1779b793b0b02d64071b18acc2f
fpontes006 <86676181+fpontes006@users.noreply.github.com>
2021-10-05 10:03:53 -0300

    alura2

--- alura01.js
+++ alura01.js
@@ -1,12 +1,27 @@
 google.charts.load('current', { 'packages': ['corechart'] })
-google.charts.setOnLoadCallback(desenharPizza)
+google.charts.setOnLoadCallback(desenharGrafico)
 
 
-function desenharPizza() {
+function desenharGrafico() {
+
+    var grafico = new google.visualization.PieChart(document.getElementById('grafico_Pizza'))
     
-    let grafico = new google.visualization.PieChart(document.getElementById('grafico_Pizza'))
+    var opcoes = {
+        title: 'Tipos de Gastos',
+        is3D: true,
+        legend: 'labeled',
+        pieSliceText: 'value',
+        slices: {
+            0: {},
+            1: { color: 'grey' },
+            2: { color: 'grey' },
+            3: {},
+            4: { offset: 0.4 },
+            5: { color: 'grey' }
+        }
+    }
 
-    const tabela = new google.visualization.DataTable();
+    var tabela = new google.visualization.DataTable();
 
     tabela.addColumn('string', 'categorias')
     tabela.addColumn('number', 'valores')
@@ -19,6 +34,33 @@ function desenharPizza() {
         ['Alimentação', 260],
 
     ])
+    grafico.draw(tabela, opcoes)
 
-    grafico.draw(tabela)
+    tabela = new google.visualization.DataTable()
+    tabela.addColumn('string', 'Mês')
+    tabela.addColumn('number', 'Gastos')
+    tabela.addRows([
+        ['jan', 400],
+        ['fev', 1100],
+        ['mar', 800],
+        ['abr', 400],
+        ['mai', 500],
+        ['jun', 750],
+        ['jul', 1500],
+        ['ago', 650],
+        ['set', 850],
+        ['out', 400],
+        ['nov', 1000],
+        ['dez', 720]
+    ])
+
+    var opcoes = {
+        title: 'Gastos Mês',
+
+    }
+
+    var grafico = new google.visualization.LineChart(document.getElementById('grafico_linha'))
+    
+
+    grafico.draw(tabela,opcoes)
 }
